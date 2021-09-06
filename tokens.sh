fdc40d31a1c66f78bca3200cb5fdb6a9af77a440
myportfolio-tech <eduardo@myportfolio.com>
2021-09-06 14:14:17 -0400

    Deleted .sh filr taht was used for testing

--- tokens.sh
+++
@@ -1,2 +0,0 @@
-curl --request POST --url 'https://YOUR_DOMAIN/oauth/token' --header 'content-type: application/x-www-form-urlencoded' 
-  --data 'grant_type=password&username=USERNAME&password=PASSWORD&audience=API_IDENTIFIER&scope=SCOPE&client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET"
